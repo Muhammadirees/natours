78e7fb1b7add8381448bc9d148e33a20eb1c7f08
Muhammad idrees <muhammadidress2000@gmail.com>
2023-06-05 11:52:56 +0500

    Changed from local to cloud database

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const app = require('./app')
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 mongoose
-   .connect(process.env.DATABASE_LOCAL, {
-      // .connect(DB, {
+   // .connect(process.env.DATABASE_LOCAL, {
+   .connect(DB, {
       useNewUrlParser: true, useUnifiedTopology: true
    })
    .then(() => console.log('DB connection Successfull'))
