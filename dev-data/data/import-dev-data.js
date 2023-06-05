6b82272b53a5cbbdcbfb333fe7dd30b7a0f36706
Muhammad idrees <muhammadidress2000@gmail.com>
2023-06-05 18:56:15 +0500

    Ready for production

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -10,8 +10,8 @@ dotenv.config({ path: './../../config.env' })
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 mongoose
-   .connect(process.env.DATABASE_LOCAL, {
-      // .connect(DB, {
+   // .connect(process.env.DATABASE_LOCAL, {
+   .connect(DB, {
       useNewUrlParser: true, useUnifiedTopology: true
    })
    .then(() => console.log('DB connection Successfull'));
