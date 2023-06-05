46cc880eaa729c4042de20c84f80ae53ca744b35
Muhammad idrees <muhammadidress2000@gmail.com>
2023-06-05 18:59:07 +0500

    Ready for production

--- app.js
+++ app.js
@@ -21,25 +21,7 @@ const globalErrorHandler = require('./controller/errorController')
 
 //Start express app
 const app = express()
-const mongoose = require('mongoose');
-const dotenv = require('dotenv')
 
-process.on('uncaughtException', err => {
-   console.log('UNCAUGHT EXCEPTION! Shutting down...........')
-   console.log(err)
-   console.log(err.name, err.message)
-   process.exit(1)
-})
-
-dotenv.config({ path: './config.env' })
-
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
-mongoose
-   // .connect(process.env.DATABASE_LOCAL, {
-   .connect(DB, {
-      useNewUrlParser: true, useUnifiedTopology: true
-   })
-   .then(() => console.log('DB connection Successfull'))
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -95,7 +77,7 @@ app.use(
    })
 )
 
-app.use(compression())
+app.use(compression)
 
 // Testing middleware
 app.use((req, res, next) => {
@@ -120,19 +102,5 @@ app.all('*', (req, res, next) => {
 
 
 app.use(globalErrorHandler)
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
-   console.log(`Listening on ${PORT}`)
-})
-
-process.on('unhandledRejection', err => {
-   console.log('UNHANDLED REJECTION! Shutting down...........')
-   console.log(err.name, err.message)
-   server.close(() => {
-      process.exit(1)
-   })
-})
 
 module.exports = app;
